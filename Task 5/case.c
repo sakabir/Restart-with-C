241ddf3080f2a655df2106a02743e025c2aac64d
Sadia Kabir Dina <sadiadina13@gmail.com>
2020-05-02 19:57:05 +0600

    Update case.c

--- Task 5/case.c
+++ Task 5/case.c
@@ -10,7 +10,9 @@ int main()
     {
       if(w[i] >= 'a' && w[i] <= 'z')
          w[i] = w[i] - 32;
-   else w[i] = w[i] + 32;
+   else if(w[i] >= 'A' && w[i] <= 'Z')
+        w[i] = w[i] + 32;
+   else printf("Not a word");
    }
    printf("%s", w);
    return 0;
